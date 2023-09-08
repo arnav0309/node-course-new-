58f441f938dfb4d638863971dffd04f55eef4e1d
Anand Kumar <arnavanand309@gmail.com>
2023-09-08 13:01:12 +0530

    testing file changes

--- testing_files/arrow-challange.js
+++ testing_files/arrow-challange.js
@@ -3,7 +3,7 @@
 // 1. Define getTasksToDo method
 // 2. Use filter to to return just the incompleted tasks (arrow function)
 // 3. Test your work by running the script
-
+// testing from MAC
 const tasks = {
     tasks: [{
         text: 'Grocery shopping',
